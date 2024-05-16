494c5208e93cc1906763b09fcbe179fc5d67321b
MartaLyrical <mhsdomingos@gmail.com>
2024-05-16 20:23:27 +0100

    layout changes

--- handcraftedhaven/pages/_app.tsx
+++ handcraftedhaven/pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Head>
-        <title>HandCrafted Haven</title>
+        <title>Web Full Stack Project</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
---
+++ handcraftedhaven/pages/index.tsx
@@ -0,0 +1,307 @@
+import type { NextPage } from "next";
+import styles from "./index.module.css";
+
+const HomeLinkcommon: NextPage = () => {
+  return (
+    <div className={styles.homeLinkcommon01}>
+      <div className={styles.shipping}>
+        <div className={styles.shippingInner}>
+          <div className={styles.groupParent}>
+            <img className={styles.frameChild} alt="" src="/group-62.svg" />
+            <div className={styles.contactUs}>CONTACT US</div>
+          </div>
+        </div>
+        <div className={styles.shippingChild}>
+          <div className={styles.freeShippingParent}>
+            <div className={styles.contactUs}>Free Shipping</div>
+            <img className={styles.frameChild} alt="" src="/group-9242.svg" />
+          </div>
+        </div>
+        <div className={styles.frameDiv}>
+          <div className={styles.groupContainer}>
+            <img className={styles.frameChild} alt="" src="/group-64.svg" />
+            <div className={styles.contactUs}>Expert ARTISANS</div>
+          </div>
+        </div>
+        <div className={styles.shippingInner1}>
+          <div className={styles.frameParent}>
+            <div className={styles.ellipseParent}>
+              <div className={styles.ellipseDiv} />
+              <div className={styles.fi5412910}>
+                <img className={styles.vectorIcon} alt="" src="/vector.svg" />
+                <img
+                  className={styles.fi5412910Child}
+                  alt=""
+                  src="/group-641.svg"
+                />
+              </div>
+            </div>
+            <div className={styles.contactUs}>Amazing Deals</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.homeLinkcommon01Inner}>
+        <div className={styles.frameGroup}>
+          <div className={styles.rectangleParent}>
+            <img
+              className={styles.rectangleIcon}
+              alt=""
+              src="/rectangle-2900@2x.png"
+            />
+            <img
+              className={styles.frameChild1}
+              alt=""
+              src="/rectangle-2902@2x.png"
+            />
+            <img
+              className={styles.frameChild2}
+              alt=""
+              src="/rectangle-2901@2x.png"
+            />
+          </div>
+          <div className={styles.frameContainer}>
+            <div className={styles.shopNowParent}>
+              <div className={styles.shopNow}>Shop now</div>
+              <img className={styles.vectorIcon1} alt="" src="/vector1.svg" />
+            </div>
+            <div className={styles.useCodeEid40Container}>
+              <p
+                className={styles.yourThreeOrders}
+              >{`Use Code Eid40 at Checkout for 40% weaver `}</p>
+              <p className={styles.yourThreeOrders}>
+                your Three Orders, end, 18-06-2024
+              </p>
+            </div>
+            <b className={styles.largeCeramicCoffeContainer}>
+              <p className={styles.yourThreeOrders}>Large Ceramic</p>
+              <p className={styles.yourThreeOrders}>Coffe Cup</p>
+            </b>
+          </div>
+        </div>
+      </div>
+      <div className={styles.showcase}>
+        <div className={styles.rectangleGroup}>
+          <img
+            className={styles.frameChild3}
+            alt=""
+            src="/rectangle-2906@2x.png"
+          />
+          <b className={styles.ceramicsJug}>
+            <p className={styles.yourThreeOrders}>{`Ceramics `}</p>
+            <p className={styles.yourThreeOrders}>Jug</p>
+          </b>
+          <div className={styles.shopNowGroup}>
+            <div className={styles.text}>Shop now</div>
+            <img className={styles.vectorIcon1} alt="" src="/vector2.svg" />
+          </div>
+        </div>
+        <div className={styles.frameParent1}>
+          <div className={styles.rectangleContainer}>
+            <img
+              className={styles.frameChild4}
+              alt=""
+              src="/rectangle-2908@2x.png"
+            />
+            <div className={styles.frameWrapper}>
+              <div className={styles.shopNowContainer}>
+                <div className={styles.text}>Shop now</div>
+                <img className={styles.vectorIcon1} alt="" src="/vector3.svg" />
+              </div>
+            </div>
+          </div>
+          <div className={styles.rectangleParent1}>
+            <img
+              className={styles.frameChild5}
+              alt=""
+              src="/rectangle-2907@2x.png"
+            />
+            <div className={styles.ceramicsDesignMugParent}>
+              <b className={styles.ceramicsDesignMugContainer}>
+                <p className={styles.yourThreeOrders}>{`Ceramics `}</p>
+                <p className={styles.yourThreeOrders}>Design mug</p>
+              </b>
+              <div className={styles.textParent}>
+                <div className={styles.text}>{` `}</div>
+                <img className={styles.vectorIcon1} alt="" src="/vector2.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.trendingProduct}>
+        <div className={styles.trendingProductParent}>
+          <b className={styles.trendingProduct1}>Trending product</b>
+          <div className={styles.sculpturesParent}>
+            <div className={styles.sculptures}>Sculptures</div>
+            <div className={styles.pottery}> Pottery</div>
+            <div className={styles.porcelain}>Porcelain</div>
+          </div>
+        </div>
+        <div className={styles.frameParent2}>
+          <div className={styles.frameParent3}>
+            <div className={styles.rectangleParent2}>
+              <div className={styles.rectangleDiv} />
+              <img
+                className={styles.frameChild6}
+                alt=""
+                src="/rectangle-2916@2x.png"
+              />
+              <div className={styles.potrayHandcraftBottleContainer}>
+                <p className={styles.yourThreeOrders}>
+                  Potray Handcraft Bottle
+                </p>
+                <p className={styles.p}>$17.89</p>
+              </div>
+              <div className={styles.buyNowWrapper}>
+                <div className={styles.buyNow}>Buy Now</div>
+              </div>
+            </div>
+            <div className={styles.rectangleParent3}>
+              <div className={styles.rectangleDiv} />
+              <img
+                className={styles.frameChild6}
+                alt=""
+                src="/rectangle-2916@2x.png"
+              />
+              <div className={styles.potrayHandcraftBottleContainer1}>
+                <p className={styles.yourThreeOrders}>
+                  Potray Handcraft Bottle
+                </p>
+                <p className={styles.p}>$17.89</p>
+              </div>
+              <div className={styles.buyNowContainer}>
+                <div className={styles.buyNow}>Buy Now</div>
+              </div>
+            </div>
+            <div className={styles.rectangleParent4}>
+              <div className={styles.rectangleDiv} />
+              <img
+                className={styles.frameChild6}
+                alt=""
+                src="/rectangle-2916@2x.png"
+              />
+              <div className={styles.potrayHandcraftBottleContainer1}>
+                <p className={styles.yourThreeOrders}>
+                  Potray Handcraft Bottle
+                </p>
+                <p className={styles.p}>$17.89</p>
+              </div>
+              <div className={styles.buyNowContainer}>
+                <div className={styles.buyNow}>Buy Now</div>
+              </div>
+            </div>
+            <div className={styles.rectangleParent5}>
+              <div className={styles.rectangleDiv} />
+              <img
+                className={styles.frameChild6}
+                alt=""
+                src="/rectangle-2916@2x.png"
+              />
+              <div className={styles.potrayHandcraftBottleContainer3}>
+                <p className={styles.yourThreeOrders}>
+                  Potray Handcraft Bottle
+                </p>
+                <p className={styles.p}>$17.89</p>
+              </div>
+              <div className={styles.buyNowContainer}>
+                <div className={styles.buyNow}>Buy Now</div>
+              </div>
+            </div>
+            <div className={styles.rectangleParent6}>
+              <div className={styles.rectangleDiv} />
+              <img
+                className={styles.frameChild6}
+                alt=""
+                src="/rectangle-2916@2x.png"
+              />
+              <div className={styles.potrayHandcraftBottleContainer3}>
+                <p className={styles.yourThreeOrders}>
+                  Potray Handcraft Bottle
+                </p>
+                <p className={styles.p}>$17.89</p>
+              </div>
+              <div className={styles.buyNowContainer}>
+                <div className={styles.buyNow}>Buy Now</div>
+              </div>
+            </div>
+            <div className={styles.rectangleParent7}>
+              <div className={styles.rectangleDiv} />
+              <img
+                className={styles.frameChild6}
+                alt=""
+                src="/rectangle-2916@2x.png"
+              />
+              <div className={styles.potrayHandcraftBottleContainer}>
+                <p className={styles.yourThreeOrders}>
+                  Potray Handcraft Bottle
+                </p>
+                <p className={styles.p}>$17.89</p>
+              </div>
+              <div className={styles.buyNowContainer}>
+                <div className={styles.buyNow}>Buy Now</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.goToSlide2Parent}>
+            <div className={styles.goToSlide2} />
+            <div className={styles.goToSlide1Parent}>
+              <div className={styles.goToSlide1} />
+              <div className={styles.goToSlide3} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.navBar}>
+        <div className={styles.frameParent4}>
+          <div className={styles.productsParent}>
+            <div className={styles.products}>Products</div>
+            <div className={styles.artisans}>Artisans</div>
+            <div className={styles.aboutUs}>About us</div>
+            <div className={styles.contact}>Contact</div>
+          </div>
+          <div className={styles.layer1}>
+            <img className={styles.groupIcon} alt="" src="/group.svg" />
+            <img className={styles.groupIcon1} alt="" src="/group1.svg" />
+          </div>
+          <div className={styles.vectorParent}>
+            <img className={styles.vectorIcon5} alt="" src="/vector4.svg" />
+            <div className={styles.fi860808Parent}>
+              <img
+                className={styles.fi860808Icon}
+                alt=""
+                src="/fi-860808.svg"
+              />
+              <div className={styles.wrapper}>
+                <div className={styles.div}>11</div>
+              </div>
+            </div>
+            <div className={styles.fi2662503}>
+              <img className={styles.layer2Icon} alt="" src="/layer-2.svg" />
+              <div className={styles.container}>
+                <div className={styles.div1}>0</div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.frameWrapper1}>
+            <div className={styles.searchParent}>
+              <div className={styles.search}>Search</div>
+              <img className={styles.fi54481Icon} alt="" src="/fi-54481.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.footerInner}>
+          <div className={styles.vectorGroup}>
+            <img className={styles.lineIcon} alt="" src="/line-11.svg" />
+            <div className={styles.copyright2024}>
+              Copyright © 2024. All Rights Reserved.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeLinkcommon;
