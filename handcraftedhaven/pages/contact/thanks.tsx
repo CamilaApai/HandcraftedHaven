7861ecceeacd2751f7d7ec91aa8ea808c8e53ae2
CamilaApai <camilaapai@hotmail.com>
2024-05-30 17:03:21 -0300

    Contact and About Pages Finish

---
+++ handcraftedhaven/pages/contact/thanks.tsx
@@ -0,0 +1,21 @@
+import Header from "../header";
+import Footer from "../footer";
+import styles from "../index.module.css";
+
+export default function Contact() {
+    return (
+        <>
+            <Header />
+            <main className={styles.mainContent}>
+                <h1>We received your email</h1>
+                <div className={styles.mainContentCenter}>
+                    <p>Thanks for sending your message.</p>
+                    <p>We will reply as soon as possible.</p>
+                </div>
+            </main>
+            <div className={styles.footerThanks}>
+                <Footer />
+            </div>
+        </>
+    );
+}
